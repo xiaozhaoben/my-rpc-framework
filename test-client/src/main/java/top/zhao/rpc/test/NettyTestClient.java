71834aea53bb1594422989d773112a8b25127651
xiaozhao <2502712438@qq.com>
2022-07-01 16:28:51 +0800

    feat: 完成客户端负载均衡

--- test-client/src/main/java/top/zhao/rpc/test/NettyTestClient.java
+++ test-client/src/main/java/top/zhao/rpc/test/NettyTestClient.java
@@ -2,6 +2,7 @@ package top.zhao.rpc.test;
 
 import top.zhao.rpc.api.HelloEntity;
 import top.zhao.rpc.api.HelloService;
+import top.zhao.rpc.loadbalancer.RandomLoadBalancer;
 import top.zhao.rpc.serializer.KryoSerializer;
 import top.zhao.rpc.transport.RpcClient;
 import top.zhao.rpc.transport.netty.client.NettyClient;
@@ -16,13 +17,14 @@ import top.zhao.rpc.transport.socket.client.SocketRpcClient;
 public class NettyTestClient {
 
     public static void main(String[] args) {
-        RpcClient client = new NettyClient();
-        client.setSerializer(new KryoSerializer());
+        RpcClient client = new NettyClient(0, new RandomLoadBalancer());
         RpcClientProxy proxy = new RpcClientProxy(client);
         HelloService helloService = proxy.getProxy(HelloService.class);
         HelloEntity helloEntity = new HelloEntity(1, "爱你");
-        String res = helloService.hello(helloEntity);
-        System.out.println(res);
+        for(int i = 0; i < 10; i ++) {
+            String res = helloService.hello(helloEntity);
+            System.out.println(res);
+        }
     }
 
 }
